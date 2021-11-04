59e6b378c53945c31646e7bf49727fe6d95f5b1d
Raymond <raymond@philippinedev.com>
2021-11-04 19:11:31 +0800

    method library with action add and remove

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :books
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :books do
+    member do
+      put "add",    to: "books#library"
+      put "remove", to: "books#library"
+    end
+  end
+
+  root to: 'books#index'
 end
